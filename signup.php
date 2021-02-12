fb562306e4ce62b46147c3404164b06936ed00f1
abhinav <abhinavawasthikvgn@gmail.com>
2021-02-12 23:47:37 +0530

    .

--- signup.php
+++ signup.php
@@ -44,98 +44,46 @@
   </div>
  
   <?php
-    
-    //connect to DB
-    $db=mysqli_connect("localhost","root","","hackerverse") ;
-
-    if(!$db){
-      die("can not connect to db".mysqli_errno());
-    }
-
-    //registration
-
-     if(isset($_POST["submit"])){
-          $par1=$_POST['mem1'];
-          $par2=$_POST['mem2'];
-          $par3=$_POST['mem3'];
-          $par4=$_POST['mem4'];
-          $par5=$_POST['mem5'];
-          
-          
-
-          $team=$_POST['team'];
-          $phone1=  $_POST['phone1'];
-          $phone2=$_POST['phone2'];
-          $colname=$_POST['colname'];
-          $email=$_POST['email'];
-          $password1=$_POST['password1'];
-          $password2=$_POST['password2'];
-
-
-          //form validation
-        /*  $errors=array();
-
-          if(empty($par1)){
-              array_push($errors,"Participant 1 is required");
-          }
-          if(empty($team)){
-              array_push($errors,"Team name is required");
-          }
-          if(empty($phone1)){
-              array_push($errors,"Phone number is required");
-          }
-          if(empty($colname)){
-              array_push($errors,"College name is required");
-          }
-          if(empty($email)){
-              array_push($errors,"Email is required");
-          }
-          if(empty($password)){
-              array_push($errors,"Password is required");
-          }
-          
-          if($password1!=$password2){
-              array_push($errors,"Password do not match");
-          }
-          
-          //check if teamName or email already exist
-
-          $team_check_query="select * from `hackerverse`.`signup` where collgeName='$colname' or Email='$email';";
-          $result=mysqli_query($db,$team_check_query);
-          $user=mysqli_fetch_assoc($result);
-
-          if($user){
-              if($user['teamName']===$team){
-                  array_push($errors,"Team name already exist");
-                }
-
-              if($user['Email']=$email){
-                  array_push($errors,"Email already exist");
-              }
-          }
-
-          //register user if no error
-
-          if(count($errors)==0){
-              $password=md5($password1);*/
-              $query="INSERT INTO `hackerverse`.`signup`(`collgeName`, `teamName`, `teamMember1`, `Email`, `Phone1`, `Phone2`, `teamMember2`, `teamMember3`, `teamMember4`, `teamMember5`) VALUES ('$colname','$team','$par1','$email','$phone1','$phone2','$par2','$par3','$par4','$par5')";
-              mysqli_query($db,$query);
-
-              if($query){
-                echo "<script type='text/javascript'> alert('Registration successful')</script>";
-              }
-              else{
-                echo "error: $query <br> $db->error";
-              }
+        $con = mysqli_connect("localhost","root","");
+        $db=mysqli_select_db($con,"hackerverse");
+        $id;
+        if (!$con) {
+            die('Could not connect: '.mysqli_errno());
+        }
+            if(isset($_POST["submit"]))
+            {
+                                
+                $institution=$_POST['colname'];
+                $teamName=$_POST['team'];
+                $name1=$_POST['mem1'];
+                $email_id=$_POST['email'];
+                $number1=$_POST['phone1'];
+                $number2=$_POST['phone2'];
+                $name2=$_POST['mem2'];
                 
-          
-    
-        } 
-    
-    
-    
+                
+                $name3=$_POST['mem3'];
+              
+                $name4=$_POST['mem4'];
+               
+                $name5=$_POST['mem5'];
+                
+                $query=mysqli_query($con,"Insert into `hackerverse`.`signup` ( `collgeName`, `teamName`, `teamMember1`, `Email`, `Phone1`, `Phone2`, `teamMember2`, `teamMember3`, `teamMember4`, `teamMember5`) values('$institution','$teamName','$name1','$email_id','$number1','$number2','$name2','$name3','$name4','$name5')");
+             
+                if($query)
+                {
+					echo "<script type = 'text/javascript'>alert('Registration Successful')</script>";
+					echo "<script type='text/javascript'>window.location.assign('index.php')</script>";
+             }
+             else
+             {
+				//echo "<script type = 'text/javascript'>alert('Registration Not Successful!! Please try again!')</script>";
+				//echo "<script type='text/javascript'>window.location.assign('index.php')</script>";
+                 echo "error: $query <br> $con->error";  
+             }
+            }
 
-?>
+        ?>
 </body>
 
 </html>
